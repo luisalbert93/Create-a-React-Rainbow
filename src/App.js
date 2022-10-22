a6c282955f8d39c8122d0cbe87c1457bd2d522fb
luis <luisdonis777@gmail.com>
2022-10-22 13:16:11 -0700

    using static data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ColorBlock from './ColorBlock';
+import ColorForm from './ColorForm';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+function App(){
+    let [colors,setColors] = useState(['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red'])
     
-    </div>
-  );
+    let colorMap = colors.map((color,i) => {
+    return (
+        <ColorBlock color={color}/>
+    )
+    })
+    const addColor = (newColor) => {
+      setColors([...colors, newColor])
+    }
+
+    return (
+      <div className="App">
+        {colors.map((color,i) =>
+        <ColorBlock key={i} color={color} />
+        )}
+        <ColorForm addColor = {addColor}/>
+      </div>
+    )
 }
 
 export default App;
